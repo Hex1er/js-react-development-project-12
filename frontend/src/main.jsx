d00bf4933f73b02a1b10ea661f9c05b982d12348
ramil <ramil1@gmail.com>
2026-08-21 06:44:24 +0400

    integrate Bugsink error tracking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import * as Sentry from '@sentry/react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css'
 import './i18n'
 import App from './App.jsx'
 import store from './slices/index.js'
+
+Sentry.init({
+  dsn: import.meta.env.VITE_BUGSINK_DSN,
+})
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
